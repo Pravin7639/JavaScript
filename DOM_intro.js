820f99c4ae293afb309b83cdf4df315d24dbf830
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-02-06 23:07:02 +0530

    DOM
    Document Object Model

---
+++ DOM_intro.js
@@ -0,0 +1,29 @@
+﻿document.write("Javascript file executed.");
+console.log("Javascript file executed.");
+
+console.log(window);
+
+function likeme() {
+    //document.write("Someone liked me");
+    //console.log("Someone liked me");
+    alert("Someone liked me");
+}
+
+
+
+console.log(document.getElementById('head').innerText);
+console.log(document.getElementById('para').innerHTML);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
