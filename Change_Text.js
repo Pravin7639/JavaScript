1ba17fb6e322446367f0eca943acd9fe5d477d76
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-02-06 23:05:40 +0530

    Change Text

---
+++ Change_Text.js
@@ -0,0 +1,46 @@
+﻿
+function changeHeading() {
+    var heading = document.getElementById('head');
+    console.log(`Heading : ${heading}`);
+
+    if (heading.innerText == 'Change Text') {
+        heading.innerText = "Student Heading";
+    } else {
+        heading.innerText = "Change Text";
+    }
+}
+
+
+
+function changeName() {
+    var name = document.getElementById('pm');
+    if (name.innerText == 'Pravin Mane') {
+        name.innerText = "Pawan Mane";
+    } else {
+        name.innerText = "Pravin Mane";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
