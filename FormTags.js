5bf8dbb1903f38c147d21c3a8588ec239d462664
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-02-06 23:12:15 +0530

    Form Tags

---
+++ FormTags.js
@@ -0,0 +1,34 @@
+﻿
+////without form tag
+//document.getElementById('submit').addEventListener("click", submitForm);
+
+//function submitForm() {
+//    var userName = document.getElementById('username').value;
+//    var password = document.getElementById('password').value;
+
+//    console.log(userName, password);
+
+//}
+
+
+
+
+//with form tag
+
+document.querySelector('#loginForm').addEventListener('submit', printValues);
+
+
+function printValues() {
+    event.preventDefault();
+    var userName = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+
+    console.log(userName, password);
+
+    document.querySelector('#head1').innerText = userName;
+}
+
+
+
+
+
